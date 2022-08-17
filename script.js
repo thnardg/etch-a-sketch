b8e455acc3728ffc04c7523566df1a88b625e9de
Thayna Rodrigues <thna.rdg@gmail.com>
2022-08-17 11:26:15 -0300

    Initial grid setup

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const board = document.getElementById("board");
 const userInput = 10; //placeholder value - will change later
 
-window.onload(makeBoard());
-
-function makeBoard(userInput) {
-  for (let i = 0; i < userInput; i++) {
-    for (let j = 0; j < userInput; j++) {
-      board.appendChild(makeDiv(board.clientWidth / userInput));
-    }
-  }
-}
+const nOfDivs = () => {
+  return board.clientWidth / userInput;
+};
 
 function createSquares(size) {
   const div = document.createElement("div");
   div.classList.add("box");
-  div.style.width = `${size}px`;
-  div.style.height = `${size}px`;
+  div.style.width = `${size}`;
+  div.style.height = `${size}`;
   return div;
 }
+
+function makeBoard(userInput) {
+  for (let i = 0; i < userInput; i++) {
+    for (let j = 0; j < userInput; j++) {
+      board.appendChild(createSquares(nOfDivs()));
+    }
+  }
+}
+
+makeBoard(10);
